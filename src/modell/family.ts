dbcc49f6722ea28ef8f96c17447066e9791c1ecd
Tim Streicher <streicher.tim@googlemail.com>
2019-10-31 11:13:25 +0100

    added family class

---
+++ src/modell/family.ts
@@ -0,0 +1,12 @@
+import {Identifiable} from "./Identifiable";
+
+export class Family extends Identifiable{
+
+    name:string
+
+    constructor(name:string){
+        super();
+        this.name = name
+    }
+
+}
