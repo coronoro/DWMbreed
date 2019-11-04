55d958670ba75a1fe87c331f1691b247b4196f01
Tim Streicher <streicher.tim@googlemail.com>
2019-11-04 22:52:45 +0100

    added fetchutil for async loading

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,48 @@
 // import choo
 
 
-import {loadFamilies, GAME, loadMonsters} from "./util/DataUtil";
+import { GAME} from "./util/DataUtil";
 import {IState} from 'choo'
 import MonsterList from "./gui/MonsterList";
 import {Monster} from "./model/game/monster/Monster";
-
-var choo = require('choo')
+import * as EventEmitter from "events";
+import Choo = require("choo");
+import {fetchBreedingRules, fetchFamilies, fetchGrowth, fetchMonsters, fetchSKills} from "./util/FetchUtil";
 var html = require('choo/html')
 
 // initialize choo
-var app = choo();
+var app = new Choo();
 
-app.use((state: IState) => {
+app.use((state: IState, emitter: EventEmitter) => {
     state.game = GAME.DWM1;
-    state.monsters = [new Monster('foo', 0),new Monster('bar', 0)]
+    state.monsters = []
+    state.families = []
+    state.skills = []
+    state.grwothTable = undefined
+    state.breedingRules = []
+
+    fetchFamilies(state.game, (data) => {
+        state.families = data;
+        fetchSKills(state.game, (data) =>{
+            state.skills = data;
+            fetchGrowth(state.game, (data) => {
+                state.growthTable = data;
+                fetchMonsters(state.game, (data) => {
+                    state.monsters = data
+                    fetchBreedingRules(state.game, (data) => {
+                        state.breedingRules = data
+                        emitter.emit('render')
+                    });
+                });
+            });
+        });
+    });
+
+
+
+
 
 })
-
-
-
-var families = loadFamilies(GAME.DWM1)
-
 
 // create a route
 app.route('/', MonsterList)
---
+++ src/util/FetchUtil.ts
@@ -0,0 +1,59 @@
+import {Family} from "../model/game/family";
+import {GrowthTable} from "../model/game/monster/GrowthTable";
+import {Skill} from "../model/game/skill/Skill";
+import {Monster} from "../model/game/monster/Monster";
+import {BreedingRule} from "../model/game/breeding/BreedingRule";
+import {GAME} from "./DataUtil";
+
+
+export function fetchAwaitJson(path:string): any{
+    let result = undefined
+    const asyncFun = async () => {
+        const response = await fetch(path)
+        const json = await response.json()
+        if(!response.ok){
+            throw new Error("HTTP error " + response.status);
+        }
+        console.log(JSON.stringify(json));
+        result = JSON.stringify(json)
+    }
+    asyncFun()
+    return result
+}
+
+export function fetchJson<T>(game: GAME, jsonFile: string, callback: (data: T) => void){
+    var url = require("../../data/"+game.toLowerCase()+"/"+jsonFile)
+    return fetch(url)
+        .then((response) => {
+            if(!response.ok){
+                throw new Error("HTTP error " + response.status);
+            }
+            return response.json()})
+        .then(data => {
+            callback(data as T)
+        })
+        .catch(error =>{
+            console.log(error);
+        });
+}
+
+export function fetchFamilies(game:GAME, callback: (data: Skill[]) => void){
+    return fetchJson(game,'family.json', callback)
+}
+
+export function fetchGrowth(game:GAME, callback: (data: GrowthTable) => void){
+    return fetchJson(game,'growth.json', callback)
+}
+
+export function fetchSKills(game:GAME, callback: (data: Skill[]) => void){
+    return fetchJson(game,'skill.json', callback)
+}
+
+export function fetchMonsters(game:GAME, callback: (data: Monster[]) => void){
+    return fetchJson(game,'monster.json', callback)
+}
+
+export function fetchBreedingRules(game:GAME, callback: (data: BreedingRule[]) => void){
+    return fetchJson(game,'breedingrules.json', callback)
+
+}
